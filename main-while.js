8aaece6375abff206d840138577af7414bf43199
Renan Lima <renangenito@hotmail.com>
2023-02-06 14:22:25 -0300

    Subindo o projeto final Alura Midi

---
+++ main-while.js
@@ -0,0 +1,16 @@
+//Somente referência
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+
+    listaDeTeclas[contador].onclick = tocaSomPom;
+    
+    contador++;
+    console.log(contador)
+}
